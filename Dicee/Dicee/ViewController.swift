7ca0ddd6d1db8a67b2f6a251d20da23afab1edfe
IncredibleQuark <lkupinski@gmail.com>
2017-08-06 23:31:25 +0200

    initial for dicee app

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Łukasz Kupiński on 06.08.2017.
+//  Copyright © 2017 Łukasz Kupiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    }
+
+}
+
