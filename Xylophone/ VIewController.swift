5d0238392822953579cd04ea66877c6a258144f9
IncredibleQuark <lkupinski@gmail.com>
2017-08-15 20:28:13 +0200

    added function to play sound

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,20 +7,25 @@
 //
 
 import UIKit
+import AudioToolbox
 
-class ViewController: UIViewController{
+class ViewController: UIViewController {
     
-
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+      
+        if let soundURL = Bundle.main.url(forResource: "note3", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            
+            AudioServicesPlaySystemSound(mySound)
+        }
     }
     
   
