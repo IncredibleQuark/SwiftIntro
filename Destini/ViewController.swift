9e05b66462ed6825a5c2cbee81314a2771db4bd7
IncredibleQuark <lkupinski@gmail.com>
2017-08-20 16:00:56 +0200

    created destiny app story conditionals

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -7,6 +7,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var storyIndex : Int = 1
+    
     // Our strings
     let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
     let answer1a = "I\'ll hop in. Thanks for the help!"
@@ -31,7 +33,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
-    // TODO Step 5: Initialise instance variables here
     
     
     
@@ -39,7 +40,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
     }
@@ -48,9 +51,51 @@ class ViewController: UIViewController {
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
     
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
+        if sender.tag == 1 && storyIndex == 1 {
+            
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+            storyIndex = 3
+        }
+        else if sender.tag == 2 && storyIndex == 1 {
+            
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+            storyIndex = 2
+        }
+            
+        else if sender.tag == 1 && storyIndex == 2 {
+            
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+            storyIndex = 3
+        }
+        
+        else if sender.tag == 2 && storyIndex == 2 {
+            storyTextView.text = story4
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+        }
+        
+        else if sender.tag == 1 && storyIndex == 3 {
+            
+            storyTextView.text = story6
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            
+        }
+            
+        else if sender.tag == 2 && storyIndex == 3 {
+            
+            storyTextView.text = story5
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+        }
+    
+     
         
     
     }
